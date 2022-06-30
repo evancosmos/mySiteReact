9917cc79549495bfac76be4f4b1997c050f76113
Evan Clark <evanclark@live.ca>
2022-06-29 18:41:14 -0700

    Removed extra files, changed css

--- src/components/MapReviews.js
+++ src/components/MapReviews.js
@@ -4,8 +4,17 @@ const MapReviews = () => {
   return (
       <div>
           <div className="reviewBox">
-            <div className="textReview"> Here are my ratings of some non-chain restraunts I've tried!</div>
-            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1rSnZHmdTc6uY6Bvg4TGqjhzF1M37umAa&ehbc=2E312F" width="640" height="480"></iframe>
+            <div className="textReview">
+              <div>Here are my ratings of some non-chain restraunts I've tried!</div>
+              <ul>
+                <li>Grey: Avoid</li>
+                <li>Black: Only if hungry</li>
+                <li>Red: Good if you're in the area</li>  
+                <li>Orange: Worth going out of your way</li>  
+                <li>Yellow: Worth planning a trip around</li>  
+              </ul>
+            </div>
+            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1rSnZHmdTc6uY6Bvg4TGqjhzF1M37umAa&ehbc=2E312F" title="Evans food reviews" width="800" height="600"></iframe>
           </div>
       </div>
   )
